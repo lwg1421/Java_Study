7d60619bf990cd81b5bc2ecd2b9f7a40fec45b45
이원근 <lwg14212naver.com>
2023-03-10 17:40:37 +0900

    스택큐

--- Algorithm/src/day2/stackQueue/QueueTest.java
+++ Algorithm/src/day2/stackQueue/QueueTest.java
@@ -1,10 +1,31 @@
 package day2.stackQueue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Queue<Integer > que = new LinkedList();
+		
+		que.offer(100);
+		que.offer(200);
+		que.offer(300);
+		que.offer(400);
+		que.offer(500);
+		System.out.println("큐에 담긴 값 출력 : " + que.toString());
+		
+		int front = que.poll();	//큐 맨 앞 값을 추출하며 삭제함과 동시에 front변수에 저장
+		System.out.println("큐의 맨 앞 값 : "+front);
+		
+		System.out.println("큐의 맨 앞 값"+que.peek());	//큐의 맨 앞 값을 삭제하지 않고 조회만
+		
+		while(! que.isEmpty()) {	//큐가 비어있을 때 까지 반복
+			System.out.println("큐 값 : " + que.poll());	// 큐의 값을 맨 앞부터 하나씩 삭제하며 추력
+			System.out.println(que);
+		}
+		
+		System.out.println(que);	//큐에 담긴 값들 출력
 	}
 
 }
---
+++ Algorithm/src/day2/stackQueue/TestTest.java
@@ -0,0 +1,10 @@
+package day2.stackQueue;
+
+public class TestTest {
+
+	public static void main(String[] args) {
+		StringBuffer result = new StringBuffer("<");
+		System.out.println(result);
+	}
+
+}
